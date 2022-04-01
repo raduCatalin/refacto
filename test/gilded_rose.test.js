7bb718d067afe334511c896ef7f2d6d210cc9e54
horeanu radu <horeanu.raducatalin.ext@eulerhermes.com>
2022-04-01 10:56:13 +0200

    refacto baby step

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -1,9 +1,21 @@
-const {Shop, Item} = require("../src/gilded_rose");
+const { Shop, Item } = require("../src/gilded_rose");
 
-describe("Gilded Rose", function() {
-  it("should foo", function() {
+describe("Gilded Rose", function () {
+  it("should return the name when 'sellIn' egal a 'quality'", function () {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
+    expect(items[0].name).toBe("foo");
+  });
+
+  it("should decresse sellIn when update quality'", function () {
+    const gildedRose = new Shop([new Item("foo", 13, 20)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toBe(12);
+  });
+
+  it("should decresse quality when update sellIn'", function () {
+    const gildedRose = new Shop([new Item("foo", 13, 20)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(19);
   });
 });
